8bf3b9c306e671bc41b66d800be9dc148e7da325
Rayanne Franco <160090372+Annfranco@users.noreply.github.com>
2025-11-10 10:52:29 -0300

    Delete apanim directory

--- apanim/src/main/java/com/example/apanim/exception/CpfJaCadastradoException.java
+++
@@ -1,7 +0,0 @@
-package com.example.apanim.exception;
-
-public class CpfJaCadastradoException extends RuntimeException {
-    public CpfJaCadastradoException(String message) {
-      super(message);
-    }
-}
