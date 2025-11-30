8b218ee7b131876536715cca292d75258e8c4bd1
Tainá Barbosa <160445609+Nabarbosa@users.noreply.github.com>
2025-11-30 02:59:41 -0300

    Add files via upload

--- apanim_react/src/main.jsx
+++ apanim_react/src/main.jsx
@@ -3,22 +3,21 @@ import { createRoot } from 'react-dom/client'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import './index.css'
 
-// Importar páginas
+// Importação das páginas
 import MenuPage from './pages/MenuPage'
 import AdocaoAnimalPage from './pages/AdocaoAnimalPage'
+import AnimaisPerdidosPage from './pages/AnimaisPerdidosPage'
+import APANIMPage from './pages/APANIMPage'
+import CompraAnimalPage from './pages/CompraAnimalPage'
+import ServicosPage from './pages/ServicosPage'
+import ParceriaPage from './pages/ParceriaPage'
+import PlanosAssinaturaPage from './pages/PlanosAssinaturaPage'
 
-// TODO: Criar estas páginas conforme necessário
-// import SobrePage from './pages/SobrePage'
 // import CadastroPage from './pages/CadastroPage'
 // import LoginPage from './pages/LoginPage'
-// import CompraAnimalPage from './pages/CompraAnimalPage'
-// import AnimaisPerdidosPage from './pages/AnimaisPerdidosPage'
 // import CadastroAnimalAdocaoPage from './pages/CadastroAnimalAdocaoPage'
 // import CadastroAnimalVendaPage from './pages/CadastroAnimalVendaPage'
 // import CadastroAnimalPerdidoPage from './pages/CadastroAnimalPerdidoPage'
-// import ParceriaPage from './pages/ParceriaPage'
-// import ServicosPage from './pages/ServicosPage'
-// import PlanosAssinaturaPage from './pages/PlanosAssinaturaPage'
 // import PerfilUsuarioPage from './pages/PerfilUsuarioPage'
 // import PerfilVendedorPage from './pages/PerfilVendedorPage'
 
@@ -27,13 +26,12 @@ createRoot(document.getElementById('root')).render(
     <BrowserRouter>
       <Routes>
         {/* Página Inicial */}
-        <Route path="/" element={<MenuPage />} />
         <Route path="/menu" element={<MenuPage />} />
         
         {/* Páginas de Animais */}
         <Route path="/adocao-animal" element={<AdocaoAnimalPage />} />
-        {/* <Route path="/compra-animal" element={<CompraAnimalPage />} /> */}
-        {/* <Route path="/animais-perdidos" element={<AnimaisPerdidosPage />} /> */}
+        <Route path="/compra-animal" element={<CompraAnimalPage />} />
+        <Route path="/animais-perdidos" element={<AnimaisPerdidosPage />} />
         
         {/* Páginas de Cadastro de Animais */}
         {/* <Route path="/cadastro-animal-adocao" element={<CadastroAnimalAdocaoPage />} /> */}
@@ -41,10 +39,10 @@ createRoot(document.getElementById('root')).render(
         {/* <Route path="/cadastro-animal-perdido" element={<CadastroAnimalPerdidoPage />} /> */}
         
         {/* Páginas Institucionais */}
-        {/* <Route path="/sobre" element={<SobrePage />} /> */}
-        {/* <Route path="/parceria" element={<ParceriaPage />} /> */}
-        {/* <Route path="/servicos" element={<ServicosPage />} /> */}
-        {/* <Route path="/planos-assinatura" element={<PlanosAssinaturaPage />} /> */}
+        <Route path="/apanim" element={<APANIMPage />} />
+        <Route path="/parceria" element={<ParceriaPage />} />
+        <Route path="/servicos" element={<ServicosPage />} />
+        <Route path="/planos-assinatura" element={<PlanosAssinaturaPage />} />
         
         {/* Páginas de Usuário */}
         {/* <Route path="/cadastro" element={<CadastroPage />} /> */}
@@ -54,6 +52,7 @@ createRoot(document.getElementById('root')).render(
         
         {/* Rota 404 - Página não encontrada */}
         <Route path="*" element={<MenuPage />} />
+        
       </Routes>
     </BrowserRouter>
   </StrictMode>,
