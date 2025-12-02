a9b9955039ce00d7a376a6c7fd2633e514f87289
Clara Esquivel <160122503+ClaraEsquivel@users.noreply.github.com>
2025-12-02 01:36:19 -0300

    Add files via upload

---
+++ apanim_react/src/pages/CadastroAnimaisPerdidosPage/index.jsx
@@ -0,0 +1,11 @@
+import CadastroAnimalPerdido from "../../components/CadastroAnimaisPerdidos";
+
+function CadastroAnimaisPerdidosPage() {
+    return (
+        <div className="cadastro-animais-perdidos">
+            <CadastroAnimalPerdido />
+        </div>
+    )
+}
+
+export default CadastroAnimaisPerdidosPage
---
+++ apanim_react/src/pages/CadastroAnimalVendaPage/index.jsx
@@ -0,0 +1,10 @@
+import CadastroAnimalVenda from "../../components/CadastroAnimalVenda";
+
+function CadastroAnimalVendaPage() {
+    return (
+        <div className="cadastro-animal-venda">
+            <CadastroAnimalVenda />
+        </div>
+    )
+}
+export default CadastroAnimalVendaPage
