ea11785202f1bb3e7c75e1e01041db66afa06096
Tainá Barbosa <160445609+Nabarbosa@users.noreply.github.com>
2025-12-02 01:41:16 -0300

    Add files via upload

---
+++ apanim_react/src/pages/LoginPage/index.jsx
@@ -0,0 +1,11 @@
+import Login from '../../components/Login'
+
+function LoginPage() {
+    return (
+        <div className='login'>
+            <Login/>
+        </div>
+    )
+}
+
+export default LoginPage
