5074cbbc57c3d472a2046fc475cc7f3949971433
Clara Esquivel <160122503+ClaraEsquivel@users.noreply.github.com>
2025-12-03 17:02:03 -0300

    Add files via upload

---
+++ src/pages/CadastroAnimalAdocaoPage/index.jsx
@@ -0,0 +1,10 @@
+import CadastroAnimalAdocao from "../../components/CadastroAnimalAdocao";
+
+function CadastroAnimalAdocaoPage() {
+    return (
+        <div className="cadastro-animal-adocao">
+            <CadastroAnimalAdocao />
+        </div>
+    )
+}
+export default CadastroAnimalAdocaoPage
