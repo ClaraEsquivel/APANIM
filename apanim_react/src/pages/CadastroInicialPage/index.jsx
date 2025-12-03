a0a472f2630a13377c96a3afaf3bab2ef8fdece8
Tainá Barbosa <160445609+Nabarbosa@users.noreply.github.com>
2025-12-03 08:06:09 -0300

    Add files via upload

---
+++ apanim_react/src/pages/CadastroInicialPage/index.jsx
@@ -0,0 +1,10 @@
+import CadastroInicial from "../../components/CadastroInicial";
+
+function CadastroInicialPage() {
+    return (
+        <div className="cadastro">
+            <CadastroInicial />
+        </div>
+    )
+}
+export default CadastroInicialPage
