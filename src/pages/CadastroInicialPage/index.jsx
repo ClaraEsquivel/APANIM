4d9665f23883944aa38a117cb0bdb7dc27b79685
Clara Esquivel <160122503+ClaraEsquivel@users.noreply.github.com>
2025-12-04 00:52:02 -0300

    Update index.jsx

---
+++ src/pages/CadastroInicialPage/index.jsx
@@ -0,0 +1,11 @@
+import CadastroInicial from '../../components/CadastroInicial'
+
+function CadastroInicialPage() {
+    return (
+        <div className='cadastro-inicial'>
+            <CadastroInicial/>
+        </div>
+    )
+}
+
+export default CadastroInicialPage
