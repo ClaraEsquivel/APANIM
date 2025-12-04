a289e52b8323814afc5db411b9fa0065d12cf109
Tainá Barbosa <160445609+Nabarbosa@users.noreply.github.com>
2025-12-04 00:57:29 -0300

    Update index.jsx

---
+++ src/pages/CadastroVendedorPage/index.jsx
@@ -0,0 +1,11 @@
+import CadastroVendedor from '../../components/CadastroVendedor'
+
+function CadastroVendedorPage() {
+    return (
+        <div className='cadastro-vendedor'>
+            <CadastroVendedor/>
+        </div>
+    )
+}
+
+export default CadastroVendedorPage
