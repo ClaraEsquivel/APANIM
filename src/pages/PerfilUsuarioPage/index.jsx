89eaf31d4ef6a9c11407737297e29dd43585db63
Clara Esquivel <160122503+ClaraEsquivel@users.noreply.github.com>
2025-12-03 22:20:41 -0300

    Update index.jsx

---
+++ src/pages/PerfilUsuarioPage/index.jsx
@@ -0,0 +1,11 @@
+import PerfilUsuario from '../../components/PerfilUsuario'
+
+function PerfilUsuarioPage() {
+    return (
+        <div className='perfil-usuario'>
+            <PerfilUsuario/>
+        </div>
+    )
+}
+
+export default PerfilUsuarioPage
