e5cdfe4e979e6fb7aeaf54d7c2353b7ee6075e81
Clara Esquivel <160122503+ClaraEsquivel@users.noreply.github.com>
2025-12-03 22:20:06 -0300

    Update index.jsx

---
+++ src/pages/CadastroUsuarioPage/index.jsx
@@ -0,0 +1,11 @@
+import CadastroUsuario from '../../components/CadastroUsuario'
+
+function CadastroUsuarioPage() {
+    return (
+        <div className='cadastro-usuario'>
+            <CadastroUsuario/>
+        </div>
+    )
+}
+
+export default CadastroUsuarioPage
